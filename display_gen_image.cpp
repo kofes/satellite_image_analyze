3740bb22a29df211618c65658efe9512051041f6
kofes <turnever@gmail.com>
2017-04-10 13:21:51 +1000

    generation and display of image done

---
+++ display_gen_image.cpp
@@ -0,0 +1,19 @@
+#include "inc/Satellite.hpp"
+#include <fstream>
+#include <iostream>
+using namespace satellite;
+
+int main ( int argc, char const *argv[] ) {
+  std::ifstream fp;
+  unsigned short width, height;
+  Image img;
+  if (argc > 1)
+    fp.open(argv[1]);
+  if (!fp.is_open())
+    fp.open("test.img");
+  fp.read(reinterpret_cast<char *>(&height), sizeof(height));
+  fp.read(reinterpret_cast<char *>(&width), sizeof(width));
+  img.read(width, height, fp);
+  img.display(width, height, 0, 0, width, height);
+  return 0;
+}
--- gen_image.cpp
+++ gen_image.cpp
@@ -8,31 +8,42 @@
 using namespace satellite;
 
 int main (int argc, char* argv[]) {
-  std::ofstream out("test.img");
+  std::ofstream out;
   unsigned short radius = 10, space = 10;
   unsigned short width = 250, height = 250, countX, countY;
+  Shape type = Shape::CIRCLE;
+  ShapeFill fill = ShapeFill::SOLID;
+  double err = 0;
   short** buff;
 
-  switch (argc) {
-    case 1 : break;
-    case 2 :
-      radius = std::strtoul(argv[1], nullptr, 10);
-    break;
-    case 3 :
-      radius = std::strtoul(argv[1], nullptr, 10);
-      space = std::strtoul(argv[2], nullptr, 10);
-    break;
-    case 4 :
-      radius = std::strtoul(argv[1], nullptr, 10);
-      space = std::strtoul(argv[2], nullptr, 10);
-      height = std::strtoul(argv[3], nullptr, 10);
-    break;
-    default :
-      radius = std::strtoul(argv[1], nullptr, 10);
-      space = std::strtoul(argv[2], nullptr, 10);
-      height = std::strtoul(argv[3], nullptr, 10);
-      width = std::strtoul(argv[4], nullptr, 10);
+//Switch argc
+  if (argc > 1)
+    out.open(argv[1]);
+  if (!out.is_open())
+    out.open("test.img");
+  if (argc > 2)
+    radius = std::strtoul(argv[2], nullptr, 10);
+  if (argc > 3)
+    space = std::strtoul(argv[3], nullptr, 10);
+  if (argc > 4)
+    height = std::strtoul(argv[4], nullptr, 10);
+  if (argc > 5)
+    width = std::strtoul(argv[5], nullptr, 10);
+  if (argc > 6) {
+    if (std::string(argv[6]) == "circle")
+      type = Shape::CIRCLE;
+    else if (std::string(argv[6]) == "square")
+      type = Shape::SQUARE;
   }
+  if (argc > 7) {
+    if (std::string(argv[7]) == "solid")
+      fill = ShapeFill::SOLID;
+    else if (std::string(argv[7]) == "default")
+      fill = ShapeFill::DEFAULT;
+  }
+  if (argc > 8)
+      err = std::strtod(argv[8], nullptr);
+//
 
   if (!(radius*space)) radius = space = 10;
   if (!width) width = 250;
@@ -51,8 +62,8 @@ int main (int argc, char* argv[]) {
 
   Image img;
   img.copy(width, height, buff);
-  img.setShapes(0, 0, width, height, radius, space, 0, Shape::CIRCLE, ShapeFill::SOLID);
-  img.display(width, height, 0, 0, width, height);
+  img.setShapes(0, 0, width, height, radius, space, err, type, fill);
+  // img.display(width, height, 0, 0, width, height);
   out.write((char*)(&(height)), sizeof(height));
   out.write((char*)(&(width)), sizeof(width));
   out < img;
