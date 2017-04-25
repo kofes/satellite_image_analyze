7e00b33ca2f865b703040ac525737d8984d04e2c
kofes <turnever@gmail.com>
2017-04-25 21:40:03 +1000

    analyze: calc double-var disttribution's characteristics

--- analyze/analyze.cpp
+++ analyze/analyze.cpp
@@ -9,14 +9,13 @@
 
 using namespace satellite;
 
-const float t_y = 3.291f;
-
 const std::string help =
   "Usage\n\n"
   "   <exec_file> [option] <input> <output>\n\n"
   "Options\n\n"
   "   -g, --gist             - Generate gist of <input>.pro to <output>.var\n"
   "   -o, --one-var          - Calculate sample mean of one distribution of <input>.var to <output>.var\n"
+  "   -d, --double-var       - Calculate sample mean of two distribution of <input>.var to <output>.var\n"
   "   -h, --help             - Print 'help'\n\n"
   "<input> <- 'test', if not setted.\n"
   "<output> <- 'test', if not setted.\n";
@@ -65,7 +64,7 @@ void generate_gist ( std::ifstream& input, std::ofstream& output ) {
 }
 
 void clac_one_dist( std::ifstream& input, std::ofstream& output ) {
-  std::vector<double> buff;
+  std::vector< std::pair<double, unsigned long> > buff;
   short min, max;
   double dh;
   unsigned long sum = 0;
@@ -80,12 +79,58 @@ void clac_one_dist( std::ifstream& input, std::ofstream& output ) {
   for (unsigned short i = 0; i < max-min+1; ++i) {
     unsigned long tmp;
     input >> tmp;
-    buff.push_back(tmp);
+    buff.push_back(std::make_pair((i+min+1)*dh, tmp));
     sum += tmp;
   }
 
-  output << math::a(buff) << ' ' << math::m(buff)*t_y/std::sqrt(sum) << std::endl;
+  output << math::first_row_moment(buff) << ' ' << math::central_moment(buff) << std::endl;
+}
 
+void clac_two_dist( std::ifstream& input, std::ofstream& output ) {
+  std::vector< std::pair<double, unsigned long> > buff;
+  short min, max;
+  double dh, E, D,
+             minus_m, plus_m,
+             minus_d, plus_d;
+
+  if (!input.is_open()) {
+    std::cerr << "Error: file isn't exist" << std::endl;
+    return;
+  }
+
+  input >> min >> max >> dh;
+
+  for (unsigned short i = 0; i < max-min+1; ++i) {
+    unsigned long tmp;
+    input >> tmp;
+    buff.push_back(std::make_pair((i+min+1)*dh, tmp));
+  }
+
+  E = math::first_row_moment(buff);
+  D = math::central_moment(buff);
+
+  minus_m = 0;
+  plus_m = max-min;
+
+  for (unsigned short i = 0; i < std::trunc(E)-min+1; ++i)
+    minus_m = (buff[minus_m].second > buff[i].second) ? minus_m : i;
+
+  for (unsigned short i = std::trunc(E)-min+1; i < buff.size(); ++i)
+    plus_m = (buff[plus_m].second > buff[i].second) ? plus_m : i;
+
+  minus_m = buff[minus_m].first;
+  minus_d = math::moment(buff, buff[minus_m].first, 2);
+
+  plus_m = buff[plus_m].first;
+  plus_d = math::moment(buff, buff[plus_m].first, 2);
+
+  if (D < minus_d + plus_d) {
+    minus_d = minus_d * ( D / (minus_d + plus_d));
+    plus_d = D - minus_d;
+  }
+
+  output << minus_m << ' ' << minus_d << std::endl;
+  output << plus_m << ' ' << plus_d << std::endl;
 }
 
 int main (int argc, char* argv[]) {
@@ -122,7 +167,19 @@ int main (int argc, char* argv[]) {
 
     return 0;
   }
+  //TWO-VAR
+  if (!std::strcmp(argv[1], "--double-var") || !std::strcmp(argv[1], "-d")) {
 
+    if (argc > 2) input.open(std::string(argv[2]) + ".var");
+    else          input.open("test.var");
+
+    if (argc > 3) output.open(std::string(argv[3]) + ".var");
+    else          output.open("test.var");
+
+    clac_two_dist(input, output);
+
+    return 0;
+  }
   std::cout << "unknown token: " << argv[1] << std::endl;
   std::cout << help << std::endl;
 
--- analyze/display.py
+++ analyze/display.py
@@ -1,16 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+t = 1.959964
+t_y = 3.983
+
 x = []
 y = []
-dh = 0
-E, D = 0, 0
+maxV, minV, dh = 0, 0, 0
+E1, D1 = 0, 0
+E2, D2 = 0, 0
 
 def phi (u):
-    return (1/np.sqrt(2*np.pi)*np.exp(-u*u/2))
+    return (1/np.sqrt(2*np.pi))*np.exp(-(u**2 / 2))
 
-def F (X):
-    return (sum(x)*dh/D)*phi((X-E)/D)
+def F (X, E, D):
+    return (dh*sum(y)/np.sqrt(D))*phi((X-E)/np.sqrt(D))
 
 with open('test.var') as fp0:
     minV, maxV, dh = [int(x) for x in next(fp0).split()]
@@ -21,14 +25,25 @@ with open('test.var') as fp0:
     plt.plot(x, y, 'k-')
 
 with open('tmp.var') as fp1:
-    E, D = [float(x) for x in next(fp1).split()]
-    print(E, D)
-    plt.plot((E, E), (0, max(y)), 'b-')
-    plt.plot((E+np.sqrt(D), E+np.sqrt(D)), (0, max(y)), 'r-')
-    plt.plot((E-np.sqrt(D), E-np.sqrt(D)), (0, max(y)), 'r-')
+    E1, D1 = [float(x) for x in next(fp1).split()]
+    E1-=1
+    E2, D2 = [float(x) for x in next(fp1).split()]
+    E2-=1
+    print(E1, D1)
+    print(E2, D2)
+    plt.plot((E1, E1), (0, max(y)), 'b--')
+    plt.plot((E1+np.sqrt(D1), E1+np.sqrt(D1)), (0, max(y)), 'g--')
+    plt.plot((E1-np.sqrt(D1), E1-np.sqrt(D1)), (0, max(y)), 'g--')
+    plt.plot((E2, E2), (0, max(y)), 'r-.')
+    plt.plot((E2+np.sqrt(D2), E2+np.sqrt(D2)), (0, max(y)), 'y-.')
+    plt.plot((E2-np.sqrt(D2), E2-np.sqrt(D2)), (0, max(y)), 'y-.')
+    D1 = (dh*sum(y) / (y[int(E1)-minV]))**2/(2*np.pi)
+    D2 = (dh*sum(y) / (y[int(E2)-minV]))**2/(2*np.pi)
+# plt.plot((E1+t*np.sqrt(D*((maxV-minV+1)/(maxV-minV))), E+t*np.sqrt(D*((maxV-minV+1)/(maxV-minV)))), (0, max(y)), 'r-')
+# plt.plot((E1-t*np.sqrt(D*((maxV-minV+1)/(maxV-minV))), E-t*np.sqrt(D*((maxV-minV+1)/(maxV-minV)))), (0, max(y)), 'r-')
 
-_X_ = np.arange(0, maxV, 1);
-_Y_ = F(_X_)
+_X_ = np.arange(0, maxV, dh);
+_Y_ = F(_X_, E1, D1) + F(_X_, E2, D2)
 
 plt.plot(_X_, _Y_)
 plt.show()
